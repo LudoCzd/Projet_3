d46c6fe6ccff42a13d3b1aad0e29a3074e4bfdc3
LudoCzd <ludo.cazade@gmail.com>
2025-01-14 14:28:34 +0100

    Fin étape 1.1, début étape 1.2. Création des filtres

--- FrontEnd/projets.js
+++ FrontEnd/projets.js
@@ -4,21 +4,36 @@ const travaux = await fetch("http://localhost:5678/api/works").then((travaux) =>
 const categories = await fetch("http://localhost:5678/api/categories").then(
   (categories) => categories.json()
 );
-
+// Création dynamiquement de la gallerie avec tous les projets//
 const gallery = document.querySelector(".gallery");
 gallery.innerHTML = "";
 for (let i = 0; i < travaux.length; i++) {
   const figure = document.createElement("figure");
-
   const imageElement = document.createElement("img");
   imageElement.src = travaux[i].imageUrl;
   const titreElement = document.createElement("figcaption");
   titreElement.innerText = travaux[i].title;
-  figure.appendChild(titreElement);
   figure.appendChild(imageElement);
-
+  figure.appendChild(titreElement);
   gallery.appendChild(figure);
 }
 
-const portfolio = document.querySelector("#porfolio");
-const divFiltres = document.createElement("div");
+const projetsCategories = document.querySelector(".projetsCategories");
+
+//Création du bouton "Tous"//
+
+const boutonTous = document.createElement("button");
+boutonTous.innerText = "Tous";
+boutonTous.classList.add("filtres");
+projetsCategories.appendChild(boutonTous);
+
+// Boucle for pour assigner les nom de catégories aux boutons //
+
+for (let i = 0; i < categories.length; i++) {
+  const boutonFiltres = document.createElement("button");
+  boutonFiltres.innerText = categories[i].name;
+  boutonFiltres.classList.add("filtres");
+  projetsCategories.appendChild(boutonFiltres);
+}
+
+const boutonTri = document.querySelectorAll(".projetsCategories button");
