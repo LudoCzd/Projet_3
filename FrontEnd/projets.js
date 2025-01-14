afb25a8a7f5d06456f8f6a57392761dd37938642
LudoCzd <ludo.cazade@gmail.com>
2025-01-14 16:56:47 +0100

    Fin de l'étape 1.2. Fonctionnement des filtres

--- FrontEnd/projets.js
+++ FrontEnd/projets.js
@@ -4,19 +4,24 @@ const travaux = await fetch("http://localhost:5678/api/works").then((travaux) =>
 const categories = await fetch("http://localhost:5678/api/categories").then(
   (categories) => categories.json()
 );
-// Création dynamiquement de la gallerie avec tous les projets//
+// Création dynamiquement de la galerie avec tous les projets//
+
 const gallery = document.querySelector(".gallery");
-gallery.innerHTML = "";
-for (let i = 0; i < travaux.length; i++) {
-  const figure = document.createElement("figure");
-  const imageElement = document.createElement("img");
-  imageElement.src = travaux[i].imageUrl;
-  const titreElement = document.createElement("figcaption");
-  titreElement.innerText = travaux[i].title;
-  figure.appendChild(imageElement);
-  figure.appendChild(titreElement);
-  gallery.appendChild(figure);
+
+function afficherProjets(projets) {
+  gallery.innerHTML = "";
+  for (let i = 0; i < projets.length; i++) {
+    const figure = document.createElement("figure");
+    const imageElement = document.createElement("img");
+    imageElement.src = projets[i].imageUrl;
+    const titreElement = document.createElement("figcaption");
+    titreElement.innerText = projets[i].title;
+    figure.appendChild(imageElement);
+    figure.appendChild(titreElement);
+    gallery.appendChild(figure);
+  }
 }
+afficherProjets(travaux);
 
 const projetsCategories = document.querySelector(".projetsCategories");
 
@@ -27,6 +32,9 @@ boutonTous.innerText = "Tous";
 boutonTous.classList.add("filtres");
 projetsCategories.appendChild(boutonTous);
 
+boutonTous.addEventListener("click", function () {
+  afficherProjets(travaux);
+});
 // Boucle for pour assigner les nom de catégories aux boutons //
 
 for (let i = 0; i < categories.length; i++) {
@@ -34,6 +42,11 @@ for (let i = 0; i < categories.length; i++) {
   boutonFiltres.innerText = categories[i].name;
   boutonFiltres.classList.add("filtres");
   projetsCategories.appendChild(boutonFiltres);
+  // fonctionnement boutons filtre //
+  boutonFiltres.addEventListener("click", function () {
+    const projetsFiltres = travaux.filter(function (projet) {
+      return projet.categoryId === categories[i].id;
+    });
+    afficherProjets(projetsFiltres);
+  });
 }
-
-const boutonTri = document.querySelectorAll(".projetsCategories button");
